fc00c178ef5455bba719ff871c9c85d811c7c25b
Leslye Soares <leslyesoares@gmail.com>
2021-11-19 21:22:10 -0300

    Resultado do quizz

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -29,7 +29,8 @@ function getQuizzes(){
 
 
 
-
+let countquizz = [0,0];
+let levelinfo;
 
 function startquizz(id){
     document.querySelector(".main-screen").classList.add("minimize");
@@ -37,11 +38,10 @@ function startquizz(id){
 
     let number_question = [0, 1, 2, 3];
     number_question.sort(()=> (Math.random() - 0.5));
-    console.log(number_question);
-    const quizz = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${2}`);
+   
+    const quizz = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${1}`);
 
     quizz.then((answer)=>{
-        console.log(answer.data.questions[0].answers);
         const screen = document.querySelector(".quizz-screen");
         let header = screen.querySelector(".title-quizz");
         header.innerHTML = `
@@ -53,6 +53,7 @@ function startquizz(id){
         let areaquestion = document.querySelector(".container-quizz");
         areaquestion.innerHTML = "";
         let answerresult = [];
+        countquizz[1] = questions.length;
         for(let i = 0; i< questions.length; i++){
             number_question.sort(()=> (Math.random() - 0.5));
             for(let j=0; j< 4; j++){
@@ -89,20 +90,59 @@ function startquizz(id){
                 </div>
             `; 
         }
+        levelinfo = answer.data.levels;
+    });
+    quizz.catch((erro)=>{
+        //console.log(erro.response);
     });
 }
 function cardselected(button, correct){
+
+    if(correct){
+        countquizz[0] += 1;
+    }
+
     if(!(button.parentNode.classList.contains("check"))){
         button.classList.add("selected");
         button.parentNode.classList.add("check");
     }
+
     setTimeout(()=>{
         const options = document.querySelectorAll(".options");
         for(let i = 0; i < options.length; i++){
             if(!(options[i].classList.contains("check"))){
                 options[i].parentNode.scrollIntoView();
+                break;
             }
         }
     }, 2000);
+    if(document.querySelectorAll(".check").length === countquizz[1]){
+        resultcalc();
+    }
+}
 
+function resultcalc(){
+    
+    setTimeout(()=>{
+        const resultarea = document.querySelector(".container-quizz");
+        let result = Math.round((countquizz[0]/countquizz[1])*100);
+        let level;
+        for(let i = 0; i <levelinfo.length; i++ ){
+            if(result > levelinfo[i].minValue){
+                level = i;
+            }
+        }
+        resultarea.innerHTML+= `
+            <div class="box-result">
+                <div class="result">${result}% de acerto: ${levelinfo[level].title}</div>
+                <div class="result-details">
+                    <img src="${levelinfo[level].image}"/>
+                    <div>${levelinfo[level].text}</div>
+                </div>
+            </div>
+        `;
+
+        document.querySelector(".box-result").scrollIntoView();
+        
+    }, 2000);
 }
