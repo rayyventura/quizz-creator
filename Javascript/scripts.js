764d80f01900ea53dea7f2f0d80d6f014d4c7bf6
Leslye Soares <leslyesoares@gmail.com>
2021-11-21 19:41:56 -0300

    Retomando

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,4 +1,30 @@
 
+const basicInfo = document.querySelector('.basic-info');
+const quizzQuestions = document.querySelector('.quizz-questions');
+const quizzLevels = document.querySelector('.level-page');
+const mainScreen = document.querySelector(".main-screen");
+const createdQuizzes = document.querySelector(".created-quizzes");
+
+function criarQuizz(){
+    basicInfo.classList.remove('minimize');
+    mainScreen.classList.add('minimize');
+}
+const finalPage = document.querySelector('.final-page');
+function tirarPag(){
+    basicInfo.classList.add('minimize');
+    quizzQuestions.classList.remove('minimize');
+}
+function tirarPag2(){
+    quizzQuestions.classList.add('minimize');
+    quizzLevels.classList.remove('minimize');
+}
+function tirarPag3(){
+    quizzLevels.classList.add('minimize');
+    finalPage.classList.remove('minimize');
+}
+function tirarPag4(){
+    finalPage.classList.add('minimize')
+}
 function showingQuizzLayout (quizz){
     const quizzCard = document.createElement("div")
     const image = `<img class="img-layout" src ="${quizz.image}">`
